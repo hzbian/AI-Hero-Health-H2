ad33856fb59c223c49212291b11b97b6a557c6fb
Tim Adler <tim@emrys-merlin.de>
2022-02-03 10:10:19 +0100

    Add umap as projection method

--- preprocess_data.py
+++ preprocess_data.py
@@ -8,6 +8,7 @@ import pandas as pd
 from joblib import dump
 from sklearn.decomposition import PCA
 from torch.utils.data import Dataset
+from umap import UMAP
 
 from dataset import CovidImageDataset
 
@@ -44,11 +45,15 @@ def ds_to_numpy(ds: Dataset) -> Tuple[np.ndarray, np.ndarray]:
               help='Number of kept PCA dimensions')
 @click.option('-s', '--seed', type=click.INT, default=137,
               help='PRNG seed')
+@click.option('-p/-np', '--pca/--no-pca', default=True, is_flag=True)
+@click.option('-u/-nu', '--umap/--no-umap', default=False, is_flag=True,)
 def main(
     data_dir: Union[str, Path],
     output_dir: Union[str, Path],
     n_dimensions: int,
-    seed: int
+    seed: int,
+    pca: bool,
+    umap: bool,
 ):
     """Project image using PCA
 
@@ -83,12 +88,6 @@ def main(
     logger.info(f'{train.shape=}')
     logger.info(f'{valid.shape=}')
 
-    pca = PCA(n_components=n_dimensions, random_state=seed)
-    train_pca = pca.fit_transform(train)
-    valid_pca = pca.transform(valid)
-    logger.info(f'{train_pca.shape=}')
-    logger.info(f'{valid_pca.shape=}')
-
     train_df: pd.DataFrame = train_ds.info_df
     valid_df: pd.DataFrame = valid_ds.info_df
 
@@ -98,18 +97,38 @@ def main(
     output_dir = Path(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    train_fn = output_dir / 'train.npy'
-    valid_fn = output_dir / 'valid.npy'
-    np.save(train_fn, train_pca)
-    np.save(valid_fn, valid_pca)
-
     train_fn = output_dir / 'train.csv'
     valid_fn = output_dir / 'valid.csv'
     train_df.to_csv(train_fn, index=False)
     valid_df.to_csv(valid_fn, index=False)
 
-    pca_fn = output_dir / 'pca.joblib'
-    dump(pca, pca_fn)
+    if pca:
+        logger.info('Applying PCA')
+        pca = PCA(n_components=n_dimensions, random_state=seed)
+        train_pca = pca.fit_transform(train)
+        valid_pca = pca.transform(valid)
+        logger.info(f'{train_pca.shape=}')
+        logger.info(f'{valid_pca.shape=}')
+
+        train_fn = output_dir / 'train.npy'
+        valid_fn = output_dir / 'valid.npy'
+        np.save(train_fn, train_pca)
+        np.save(valid_fn, valid_pca)
+
+        pca_fn = output_dir / 'pca.joblib'
+        dump(pca, pca_fn)
+
+    if umap:
+        logger.info('Applying UMAP')
+        umap = UMAP(n_components=min(n_dimensions, 100))
+        train_umap = umap.fit_transform(train)
+        valid_umap = umap.transform(valid)
+        logger.info(f'{train_umap.shape=}')
+        logger.info(f'{valid_umap.shape=}')
+
+        np.save(output_dir / 'train_umap.npy', train_umap)
+        np.save(output_dir / 'valid_umap.npy', valid_umap)
+        dump(umap, output_dir / 'umap.joblib')
 
 
 if __name__ == "__main__":
